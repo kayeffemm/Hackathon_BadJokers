2dad2bb7ff83e6bc43701ae924c2f65109624bba
Lina0596 <lina.dahlhaus@icloud.com>
2025-01-21 13:39:34 +0100

    define functions

---
+++ joke_api.py
@@ -0,0 +1,36 @@
+import requests
+
+def get_all_categories():
+    """
+    Gets all the available categories from the API and puts them in a list.
+    :return: list of categories
+    """
+    pass
+
+
+def get_joke():
+    """
+    Gets a random joke from the api.
+    :return: joke as a string
+    """
+    pass
+
+
+def get_category(category):
+    """
+    Gets a joke from the chosen category.
+    :param category: chosen category from menu
+    :return: joke as a string
+    """
+    pass
+
+
+def get_keyword(keyword):
+    """
+    Gets a joke which contains the chosen keyword.
+    :param keyword: chosen keyword
+    :return: joke as a string
+    """
+    pass
+
+
