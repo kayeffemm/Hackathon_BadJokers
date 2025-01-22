333f41b70c742fa88648a4d2c0239520025ca421
lutzoschkinat <oschkinatl7@gmail.com>
2025-01-22 12:26:48 +0100

    dict in json

--- json_manager.py
+++ json_manager.py
@@ -14,7 +14,34 @@ def dict_store(filepath):
     for key, value in loaded_dict.items():
         print(f"{key}: {value}")
 
-def content_json
 
-dict_store()
+
+def content_json(filepath, key="number"):
+    """
+    Extrahiert den Wert eines bestimmten Schlüssels aus einer JSON-Datei.
+
+    Args:
+        filepath (str): Der Pfad zur JSON-Datei.
+        key (str, optional): Der Schlüssel, dessen Wert extrahiert werden soll. Defaults to "joke".
+
+    Returns:
+        str: Der Wert des angegebenen Schlüssels oder None, wenn der Schlüssel nicht gefunden wurde.
+    """
+
+    try:
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+            return data.get(key)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Fehler beim Lesen der JSON-Datei: {e}")
+        return None
+
+# Beispielaufruf:
+joke_content = content_json("jokes.json")
+if joke_content:
+    print(joke_content)
+else:
+    print("Witz konnte nicht gefunden werden.")
+
+
 
